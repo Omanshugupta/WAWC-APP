268ac77788c50230544861d714640e6d3e7a3be1
Shr123isti <shristi19440@gmail.com>
2024-09-03 20:46:12 +0530

    education2 added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react'
+
 import Navbar from './Components/Navbar'
 import Home from './Components/Home'
 import Education from './Components/Education'
+import Edu from './Components/Edu' 
 export default function App() {
   return (
     <div>
       <Navbar/>
       <Home/>
       <Education/>
+      <Edu/>
     </div>
   )
 }
---
+++ frontend/src/Components/Edu.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import Imge from './assets/img.jpg';
+import './Style.css'
+
+
+function Edu() {
+  return (
+    <div className="main">
+      <div className="heading">
+      <h1 style={{textDecorationLine: 'underline'}}>EDUCATION</h1>
+      <h3 style={{color: "Darksalmon"}}>Education in Beauty Therapy</h3>
+      </div>
+      <div className="para" >
+      <p style={{padding:'30px'}}>We offer a range of beauty and spa qualifications that are highly valued by employers worldwide.
+      <br/><br/><br/>
+      Our flexible programs, available for full-time, part-time, and online study, cater to diverse lifestyles and career goals.<br/> Whether you're a beginner or an experienced professional, we have the perfect course to elevate your skills and career prospects.</p>
+      <div className="pic"><img src={Imge} height={250} width={400}/></div>
+      </div>
+      <br/>
+      <br/>
+      <div className="button1">
+        <div className="buttoni"><a href="#Diplomas">Diplomas</a></div>
+        <div className="buttonii"><a href="#Certificates">Certificates</a></div>
+        <div className="buttoniii"><a href="#Industry Guides">Industry Guides</a></div>
+      </div>
+      <br/>
+      <br/>
+      <br/>
+      <div className="button2">
+        <div className="buttonxi"><a href="#Course Details">Course Details</a></div>
+        <div className="buttonxii"><a href="#Application Forms">Application Forms</a></div>
+        <div className="buttonxiii"><a href="#Subscribe Newsletter">Subscribe Newsletter</a></div>
+      </div>
+      
+    </div>
+     
+  );
+}
+
+export default Edu;
